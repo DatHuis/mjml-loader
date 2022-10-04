fa2b2aaff38f212aa5605145cde256d2e9586052
Ramon Gebben <ramon@dathuis.nl>
2022-10-04 13:41:34 +0200

    Updated all outdated deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:jest/recommended',
   ],
   env: {
